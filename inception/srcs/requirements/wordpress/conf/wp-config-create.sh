7774414966fcbf3d5de3cd325aaed7d404e17b4f
mkulikov <mkulikov@student.42berlin.de>
2025-12-25 20:52:49 +0100

    update wp-config create script

--- inception/srcs/requirements/wordpress/conf/wp-config-create.sh
+++ inception/srcs/requirements/wordpress/conf/wp-config-create.sh
@@ -2,6 +2,7 @@
 set -e
 
 WEBROOT="/var/www/html"
+: "${DOMAIN_NAME:?DOMAIN_NAME is not set (e.g. DOMAIN_NAME=localhost:8443 or login.42.fr)}"
 
 echo "Waiting for MariaDB..."
 until mysqladmin ping -h"${DB_HOST:-mariadb}" -u"${DB_USER}" -p"${DB_PASS}" --silent; do
@@ -25,6 +26,14 @@ if [ ! -f "$WEBROOT/wp-config.php" ]; then
   sed -i "s/username_here/${DB_USER}/" "$WEBROOT/wp-config.php"
   sed -i "s/password_here/${DB_PASS}/" "$WEBROOT/wp-config.php"
   sed -i "s/localhost/${DB_HOST:-mariadb}/" "$WEBROOT/wp-config.php"
+fi
+
+# Force correct URL (idempotent)
+WP_URL="https://${DOMAIN_NAME}"
+if ! grep -q "WP_HOME" "$WEBROOT/wp-config.php"; then
+  sed -i "/That's all, stop editing!/i define('WP_HOME', '${WP_URL}');\ndefine('WP_SITEURL', '${WP_URL}');\n" \
+    "$WEBROOT/wp-config.php" \
+  || echo "Warning: Could not inject WP_HOME/WP_SITEURL (anchor not found)."
 fi
 
 chown -R www-data:www-data "$WEBROOT"
